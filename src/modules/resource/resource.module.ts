23ee3f488c162c8269ba4fcdb9246cb8e5d002d7
jadSalameh <jjadsalamehh@gmail.com>
2025-05-18 21:29:43 -0400

    import organization module

--- src/modules/resource/resource.module.ts
+++ src/modules/resource/resource.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResourceService } from './resource.service';
 import { ResourceController } from './resource.controller';
 import { Resource } from '../entities/resource.entity';
+import { OrganizationModule } from '../organization/organization.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Resource])],
+  imports: [TypeOrmModule.forFeature([Resource]), OrganizationModule],
   controllers: [ResourceController],
   providers: [ResourceService],
   exports: [ResourceService],
