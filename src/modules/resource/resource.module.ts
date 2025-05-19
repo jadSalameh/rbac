53a85a61494cdd2b099a4553d330d24a96ed54a1
jadSalameh <jjadsalamehh@gmail.com>
2025-05-18 21:51:59 -0400

    resource access control implemented

--- src/modules/resource/resource.module.ts
+++ src/modules/resource/resource.module.ts
@@ -4,9 +4,14 @@ import { ResourceService } from './resource.service';
 import { ResourceController } from './resource.controller';
 import { Resource } from '../entities/resource.entity';
 import { OrganizationModule } from '../organization/organization.module';
+import { User } from '../entities/user.entity';
+import { Organization } from '../entities/organization.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Resource]), OrganizationModule],
+  imports: [
+    TypeOrmModule.forFeature([Resource, User, Organization]),
+    OrganizationModule,
+  ],
   controllers: [ResourceController],
   providers: [ResourceService],
   exports: [ResourceService],
